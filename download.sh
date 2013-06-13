6094b82742ec0ef0379ce66525b5fe19b3372328
Thomas Levine <occurrence@thomaslevine.com>
2013-06-13 17:54:22 -0400

    download script

---
+++ download.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+wget -O chicago.csv https://data.cityofchicago.org/api/views/jyb9-n7fm/rows.csv?accessType=DOWNLOAD
+wget -O newyork.csv https://github.com/ajschumacher/datathon/raw/master/resultsDaily.csv
