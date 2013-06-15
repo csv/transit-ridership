5740c85a3487452c55637c7d876e4bc290eae4f8
Thomas Levine <occurrence@thomaslevine.com>
2013-06-15 14:44:17 -0700

    mix wavs

--- merge.sh
+++ merge.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 bpm=140
 
-ffmpeg -r $(perl -e "print $bpm/60;") -i frames/%04d.png -i transit.mp3 -qscale 0 transit.webm
+sox -M chicago.wav newyork.wav beat.wav /tmp/transit.wav
+ffmpeg -r $(perl -e "print $bpm/60;") -i frames/%04d.png -i /tmp/transit.wav -qscale 0 transit.webm
 
