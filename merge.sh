f1fba297217b82ce64a2aec1fa028bb2d7bfa261
Thomas Levine <occurrence@thomaslevine.com>
2013-06-15 18:09:57 -0400

    fudge factor of 2 to make the tracks line up

--- merge.sh
+++ merge.sh
@@ -2,6 +2,6 @@
 set -e
 bpm=140
 
-sox -M chicago.wav newyork.wav beat.wav /tmp/transit.wav
-ffmpeg -r $(perl -e "print $bpm/60;") -i frames/%04d.png -i /tmp/transit.wav -qscale 0 transit.webm
+test -e /tmp/transit.wav || sox -M chicago.wav newyork.wav beat.wav /tmp/transit.wav
+ffmpeg -r $(perl -e "print 2 * $bpm / 60;") -i frames/%04d.png -i /tmp/transit.wav -qscale 0 transit.webm
 
