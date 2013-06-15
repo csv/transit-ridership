aaaaf30b42cb8e4066efe37f1582548f87142f4c
Thomas Levine <occurrence@thomaslevine.com>
2013-06-15 01:04:03 -0400

    qscale

--- merge.sh
+++ merge.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 bpm=140
 
-ffmpeg -r $(perl -e "print $bpm/60;") -i frames/%04d.png -i transit.mp3 -sameq transit.webm
+ffmpeg -r $(perl -e "print $bpm/60;") -i frames/%04d.png -i transit.mp3 -qscale 0 transit.webm
 
