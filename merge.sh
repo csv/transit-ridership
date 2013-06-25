3c8b795243ec10d3234e595de535d9fc0bb7a1ec
Thomas Levine <occurrence@thomaslevine.com>
2013-06-25 18:47:07 -0400

    different rate

--- merge.sh
+++ merge.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 set -e
-bpm=140
 
 test -e /tmp/transit.wav || sox -M chicago.wav newyork.wav beat.wav /tmp/transit.wav
-ffmpeg -r $(perl -e "print 2 * $bpm / 60;") -i frames/%04d.png -i /tmp/transit.wav -qscale 0 transit.webm
+
+bpm=140
+# rate=$(perl -e "print 2 * $bpm / 60;")
+rate=$(perl -e "print 1036 / (4 * 60 + 13.71);")
+
+ffmpeg -r $rate -i frames/%04d.png -i /tmp/transit.wav -q:v 0 transit.webm
 
