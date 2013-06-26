fe536f193ca3104de711713998f2c312168dfb7a
Thomas Levine <occurrence@thomaslevine.com>
2013-06-26 02:06:22 -0400

    framerate

--- merge.sh
+++ merge.sh
@@ -5,7 +5,7 @@ test -e /tmp/transit.wav || sox -M chicago.wav newyork.wav beat.wav /tmp/transit
 
 bpm=140
 # rate=$(perl -e "print 2 * $bpm / 60;")
-rate=$(perl -e "print ($(wc -l newyork.csv) - 1) / (4 * 60 + 13.71);")
+rate=$(perl -e "print 1036 / (4 * 60 + 13.71);")
 
-ffmpeg -r $rate -i frames/%04d.png -i /tmp/transit.wav -q:v 0 transit.webm
+ffmpeg -r $rate -i frames/%04d.png -i /tmp/transit.wav -qscale 0 transit.webm
 
